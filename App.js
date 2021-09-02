f1474f6da204c60094a2fc28bd46e5eab982292b
rafayasad <asadrafay998@gmail.com>
2021-09-02 21:14:13 +0500

    adds update option

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 export default function App(){
   const [text, setText] = useState(null);
   const [todos,setTodos] = useState([])
+  const [isEdit,setIsEdit] = useState(false)
+  const [currentIndex,setCurrentIndex] = useState(null)
+  const [isDisabled,setIsDisabled] = useState(false)
 
   const addItem = () =>{
     const tempTodo = [...todos]
@@ -19,6 +22,24 @@ export default function App(){
   const deleteItem = (i) =>{
     todos.splice(i,1)
     setTodos([...todos])
+  }
+
+  const editItem = (i) =>{
+    setText(todos[i])
+    setIsEdit(true)
+    setIsDisabled(true)
+    setCurrentIndex(i)
+  }
+
+  const updateItem = () =>{
+    setIsDisabled(false)
+    setIsEdit(false)
+    const tempTodo = [...todos]
+    tempTodo.splice(currentIndex,1,text)
+    setTodos(tempTodo)
+    console.log(tempTodo)
+    setCurrentIndex(null)
+    setText('')
   }
 
   return(
@@ -45,10 +66,30 @@ export default function App(){
 
       <IconButton
     icon={require('./assets/delete.png')}
-    color='blue'
+    color='rgb(113, 86, 158)'
     size={20}
     onPress={() => deleteItem(index)}
       />
+
+  {isDisabled ?
+  
+  <IconButton
+  icon={require('./assets/edit.png')}
+  color='rgb(113, 86, 158)'
+  size={20}
+  onPress={()=>editItem(index)}
+  disabled={true}
+  />
+:
+<>
+<IconButton
+  icon={require('./assets/edit.png')}
+  color='rgb(113, 86, 158)'
+  size={20}
+  onPress={()=>editItem(index)}
+  />
+</>
+}
 
       </View>
       )
@@ -58,12 +99,24 @@ export default function App(){
       </ScrollView>
 
     <View style={{alignSelf:'center'}}>
+      
+      {isEdit ?
+      <FAB
+      style={styles.fab}
+      large
+      icon={require('./assets/update.png')}
+      onPress={updateItem}
+      />
+
+      :
+      
       <FAB
       style={styles.fab}
       large
       icon={require('./assets/plus.png')}
       onPress={addItem}
-  />
+      />
+    }
       </View>
     </SafeAreaView>
   )
